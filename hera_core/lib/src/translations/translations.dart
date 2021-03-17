c4f741150d89195c6ad69dda63dc9e2a7207085d
Atoumbre <atoumbre@gmail.com>
2021-03-17 06:32:45 +0000

    Update gitignore

--- hera_core/lib/src/translations/translations.dart
+++
@@ -1,7 +0,0 @@
-import 'package:hera_core/src/translations/locales/english.dart';
-import 'package:hera_core/src/translations/locales/french.dart';
-
-Map<String, Map<String, String>> translations() => {
-      'en': english(),
-      'fr': french(),
-    };
--- hera_core/test/monnaie_core_test.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  // test('adds one to input values', () {
-  //   var user = TUser(fullname: '15', id: '18');
-
-  //   print(user.getId());
-  // });
-}
